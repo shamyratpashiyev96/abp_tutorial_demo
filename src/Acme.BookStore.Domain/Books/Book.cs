fe5b937f677f653a82347e07abc72e33f74c0e7d
shamyratpashiyev96 <shamyratpashiyev96@gmail.com>
2023-10-20 14:53:01 +0500

    Finished Part 8:Application Layer (Added test for Authors and Added Required tag to Book in order to fix test error)

--- src/Acme.BookStore.Domain/Books/Book.cs
+++ src/Acme.BookStore.Domain/Books/Book.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using Volo.Abp.Domain.Entities;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Acme.BookStore.Books
